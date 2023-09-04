63fedb1100017e2c637636eeaeaf5a8123848cea
Nam Chan <namanhseoul@gmail.com>
2023-09-04 15:35:33 +0700

    update

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ({
     selector: 'app-location',
-    templateUrl: './location.component.html'
+    templateUrl: './location.component.html',
+    styleUrls: ['./location.component.css']
 })
 
 export class LocationComponent {
